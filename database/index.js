4feeb282b184223cd7b0b0a9fb51b0b72374134d
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2018-09-04 20:29:05 -0500

    update database index, package.json files

--- database/index.js
+++ database/index.js
@@ -1,2 +1,18 @@
 var mysql = require('mysql');
-var mongo= rewuire('mongo')
+
+var connection = mysql.createConnection({
+  host
+  user
+  password
+  database
+});
+
+var selectAll = function(callback) {
+  connection.query('SELECT * FROM '/*database*/, function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    }else{
+      callback(null, results);
+    }
+  });
+};
